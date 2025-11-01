7d31c5c9da4ca7d41d545db45b312d5b2fa0bbd5
vijaydasp <vijaydasd2@gmail.com>
2025-11-02 03:52:55 +0530

    Fix: missing datas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
+  optimizeDeps: {
+    include: ['@babel/runtime', '@babel/runtime/helpers/extends', '@babel/runtime/helpers/createSuper']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
 })
