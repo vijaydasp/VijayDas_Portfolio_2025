b8d6471f2e52709f2735bd2de7b2d60969b1525f
vijaydasp <vijaydasd2@gmail.com>
2025-11-02 04:01:51 +0530

    Fix Vite build and Netlify routing

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: './',
+  resolve: {
+    alias: {
+      '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
+      '@babel/runtime/helpers': path.resolve(__dirname, 'node_modules/@babel/runtime/helpers')
+    }
+  },
   optimizeDeps: {
-    include: ['@babel/runtime', '@babel/runtime/helpers/extends', '@babel/runtime/helpers/createSuper']
+    include: [
+      '@babel/runtime',
+      '@babel/runtime/helpers/extends',
+      '@babel/runtime/helpers/createSuper',
+      '@babel/runtime/helpers/objectWithoutPropertiesLoose',
+      '@babel/runtime/helpers/typeof'
+    ]
   },
   build: {
     commonjsOptions: {
+      transformMixedEsModules: true,
       include: [/node_modules/],
     },
   },
