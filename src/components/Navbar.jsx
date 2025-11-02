3e8697a213b6b41aa9b8e5e3cc1993005c4fa473
vijaydasp <vijaydasd2@gmail.com>
2025-11-02 23:19:50 +0530

    Updated Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -141,12 +141,12 @@ const Navbar = () => {
     
         {/* Mobile Menu Overlay */}
         <div
-            className={`md:hidden h-2/5 fixed inset-0 bg-[#030014] transition-all duration-300 ease-in-out ${
+            className={`md:hidden fixed inset-0 bg-[#030014] transition-all duration-300 ease-in-out ${
                 isOpen
                     ? "opacity-100 translate-y-0"
                     : "opacity-0 translate-y-[-100%] pointer-events-none"
             }`}
-            style={{ top: "64px" }}
+            style={{ top: "64px", height: "calc(100vh - 64px)" }}
         >
             <div className="flex flex-col h-full">
                 <div className="px-4 py-6 space-y-4 flex-1 ">
